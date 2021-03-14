a42dc279b9c56eb0436e15d01141a1d51a4ab2a3
Alexandr Eremin <tik777@rambler.ru>
2021-03-14 18:23:57 +0300

    StringBuilder

--- src/GenNumber.java
+++ src/GenNumber.java
@@ -3,23 +3,23 @@ import java.util.ArrayList;
 /**
  * Created by root on 11.03.2021.
  */
-public class GenNumber {
-    StringBuilder bil = new StringBuilder();
+class GenNumber {
+
     ArrayList<String> ListNumber = new ArrayList<>();
-    String[] letterOne = {"a","b","c","d"};
+    private String[] letterOne = {"a","b","c","d"};
 
     public ArrayList<String> getListNumber() {
         for (int i = 0; i< letterOne.length; i++) {
             for (LetterTwo letterTwo : LetterTwo.values()) {
-                for (int j = 0; j <= 150; j++) {
+                for (int j = 0; j <= 10; j++) {
+                    StringBuilder bil = new StringBuilder();
                     bil.append(letterOne[i]);
                     bil.append("-");
                     bil.append(letterTwo.toString());
                     bil.append("-");
                     bil.append(j);
-                    bil.append("\n");
+                  //  bil.append("\n");
 //                   System.out.println(letterOne + "-" + letterTwo + "-" + i);
-
                     ListNumber.add(bil.toString());
                   //  ListNumber.add(letterOne[i] + "-" + letterTwo + "-" + j);
                 }
